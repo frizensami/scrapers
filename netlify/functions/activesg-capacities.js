800f82d1a77c7b3ead9217228e5587f6244f7016
Sriram Sami <frizensami@gmail.com>
2025-03-09 21:43:01 +0800

    Fix syntax error

--- netlify/functions/activesg-capacities.js
+++ netlify/functions/activesg-capacities.js
@@ -50,7 +50,7 @@ async function extractGymCapacities(url) {
     }
     const timestamp = all_data.timestamp;
     // Format 
-    const human_timestamp = new Date(unix_timestamp).toSOString();
+    const human_timestamp = new Date(timestamp).toSOString();
 
     const swim = all_data.swimFacilities.map((fac) => {
       if (!fac.hasOwnProperty("name") || !fac.hasOwnProperty("isClosed") || !fac.hasOwnProperty("capacityPercentage")) {
