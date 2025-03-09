34676a3864ea7cafc04d948f35dd10c80c40c56e
Sriram Sami <frizensami@gmail.com>
2025-03-09 21:50:51 +0800

    Force timezone singapore

--- netlify/functions/activesg-capacities.js
+++ netlify/functions/activesg-capacities.js
@@ -50,7 +50,7 @@ async function extractGymCapacities(url) {
     }
     const timestamp = all_data.timestamp;
     // Format 
-    const human_timestamp = new Date(timestamp).toString();
+    const human_timestamp = new Date(timestamp).toLocaleString('en-SG', { timeZone: 'Asia/Singapore' });
 
     const swim = all_data.swimFacilities.map((fac) => {
       if (!fac.hasOwnProperty("name") || !fac.hasOwnProperty("isClosed") || !fac.hasOwnProperty("capacityPercentage")) {
